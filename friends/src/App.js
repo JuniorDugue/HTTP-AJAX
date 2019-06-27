988b9431668ca46e3e659ee1f869cd6c54da7ce2
Junior Dugue <jrdugue@gmail.com>
2019-06-27 19:54:25 -0400

    converted functional component  to class component, added key and fixed my mapping bug to my friend component, passed axios in cdm

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Friend from "./components/Friend";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Friend/>
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <h1>My Friendlist</h1>
+      <Friend
+            friends={this.state.friends}
+          />
     </div>
   );
+}
 }
 
 export default App;
--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Friend = (props) => {
-  return(
+  return (
     <div>
-      {props.friends.map((friend => 
-        <div>
-          <h1>{friend.name}</h1>
-          <p>Age: {friend.age}</p>
-          <p>Email: {friend.email}</p>
-        </div>
-      ))}
+      {props.friends.map(friend => {
+        return (
+          <div key={friend.id}>
+            <p>Name: {friend.name}</p>
+            <p>Age: {friend.age}</p>
+            <p>Email: {friend.email}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
