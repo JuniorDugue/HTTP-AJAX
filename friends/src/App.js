2d2ff11ae07db08c077778c77579d19a147940cd
Junior Dugue <jrdugue@gmail.com>
2019-06-27 21:28:32 -0400

    added addfriend component, fixed few typos that were giving me error msgs,still have to work on the styling but was focusing on the functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Friend from "./components/Friend";
+import AddFriend from "./components/AddFriend";
 import axios from "axios";
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   constructor(){
@@ -20,13 +22,40 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  addFriend = data => {
+    axios
+      .post("http://localhost:5000/friends", data)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  deleteFriend = id => {
+    axios
+      .delete(`http://localhost:5000/friends/${id}`)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   render(){
     return (
       <div className="App">
       <h1>My Friendlist</h1>
       <Friend
-            friends={this.state.friends}
-          />
+        friends={this.state.friends}
+        deleteFriend={this.deleteFriend}
+      />
+      <Route
+        path='/add-friend'
+        render={(props) => <AddFriend addFriend={this.addFriend} />}
+      />
     </div>
   );
 }
--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -9,6 +9,9 @@ const Friend = (props) => {
             <p>Name: {friend.name}</p>
             <p>Age: {friend.age}</p>
             <p>Email: {friend.email}</p>
+              <div onClick={() => props.deleteFriend(friend.id)}>
+                Delete Friend?
+              </div>
           </div>
         )
       })}
