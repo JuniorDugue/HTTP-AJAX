c5df0d18351a9523d33c30e8e01a0550a9ad38fe
Junior Dugue <jrdugue@gmail.com>
2019-06-27 21:31:06 -0400

    saved work

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Friend from "./components/Friend";
 import AddFriend from "./components/AddFriend";
 import axios from "axios";
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       friends: []
-    }
+    };
   }
 
   componentDidMount() {
     axios
       .get("http://localhost:5000/friends")
       .then(response => {
-        this.setState({ friends: response.data })
+        this.setState({ friends: response.data });
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
 
   addFriend = data => {
@@ -28,10 +28,10 @@ class App extends Component {
       .then(response => {
         this.setState({
           friends: response.data
-        })
+        });
       })
       .catch(err => console.log(err));
-  }
+  };
 
   deleteFriend = id => {
     axios
@@ -39,26 +39,23 @@ class App extends Component {
       .then(response => {
         this.setState({
           friends: response.data
-        })
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
-  render(){
+  render() {
     return (
       <div className="App">
-      <h1>My Friendlist</h1>
-      <Friend
-        friends={this.state.friends}
-        deleteFriend={this.deleteFriend}
-      />
-      <Route
-        path='/add-friend'
-        render={(props) => <AddFriend addFriend={this.addFriend} />}
-      />
-    </div>
-  );
-}
+        <h1>My Friendlist</h1>
+        <Friend friends={this.state.friends} deleteFriend={this.deleteFriend} />
+        <Route
+          path="/add-friend"
+          render={props => <AddFriend addFriend={this.addFriend} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
